beb2b1cd1d6b38a3e1cd54b9480d5b86f950f864
Priscila Caimi <priscila.caimi@hotmail.com>
2024-07-30 21:39:05 -0300

    aula com adição de report

--- cypress.config.js
+++ cypress.config.js
@@ -4,4 +4,11 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://todomvc.com/examples/react/dist/'
   },
+  reporter: "mochawesome",
+  reporterOptions: {
+    overwrite: false,
+    html: true,
+    json: false,
+    reportDir: "results/"
+  }
 });
